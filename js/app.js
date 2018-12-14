a47584ba49c2928d28e59084163493df3fe2312d
Walter Goins <wgoinz@gmail.com>
2018-12-14 15:51:34 -0500

    Added more logic for game. ShowSequence () will show the sequence and animate it accordingly. buttonisClicked () will animate the corresponding div when it is clicked. (this needs to be set on an interval so that it does not stay clicked) When the button is clicked, the data-color is recorded and pushed into an array. The compareSequences () compares the lengths of the arrays and the values stored within them. The playerScore is ++ through each iteration. This is checked each time a player clicks the corresponding button (not the final logic)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 console.log("Start of Simon Says")
 
-// let selectGreen = document.getElementById('green')
-// let selectYellow = document.getElementById('yellow')
-// let selectRed = document.getElementById('red')
-// let selectBlue = document.getElementById('blue')
+let selectGreen = document.getElementById('green')
+let selectYellow = document.getElementById('yellow')
+let selectRed = document.getElementById('red')
+let selectBlue = document.getElementById('blue')
 
 let playerScore = 0
 let cpuScore = 0
@@ -14,53 +14,58 @@ let cpuSequence = []
 let cpuChoose = ['green', 'red', 'blue', 'yellow']
 let randomColor
 
-function stopInt() {
-
-}
-
 function pickRandomColor() {
     console.log('Game has begun')
     randomColor = cpuChoose[Math.floor(Math.random() * cpuChoose.length)]
     cpuSequence.push(randomColor)
-
     console.log(randomColor)
     console.log(cpuSequence)
-    function showSequence() {
-        for (i = 0; i < cpuSequence.length; i++) {
-            let selectRandom = document.getElementById(cpuSequence[i])
-            function addSelectedClass() {
-                selectRandom.classList.add('selected')
-            }
-            let startInterval = setInterval(addSelectedClass, 1000)
-            setTimeout(function () {
-                clearInterval(startInterval)
-                selectRandom.classList.remove('selected')
-            }, 2000)
-        }
-    }
-
     showSequence()
-    return randomColor
 
+    return randomColor
+}
+
+function buttonIsClicked() {
+    this.classList.add('clicked')
+    playerSequence.push(this.getAttribute('data-color'))
+    compareSequences()
+}
+
+function compareSequences() {
+    console.log(playerSequence)
+    console.log(cpuSequence)
+    for (i = 0; i < cpuSequence.length; i++)
+        if (playerSequence[i] === cpuSequence[i]) {
+            playerScore++
+            console.log('The score is' + playerScore)
+            pickRandomColor()
+        } else {
+            alert('not equal')
+            playerSequence = []
+            cpuSequence = []
+            pickRandomColor()
+        }
 }
 
 function showSequence() {
     for (i = 0; i < cpuSequence.length; i++) {
+        let selectRandom = document.getElementById(cpuSequence[i])
+        function addSelectedClass() {
+            selectRandom.classList.add('selected')
+        }
         let startInterval = setInterval(addSelectedClass, 1000)
         setTimeout(function () {
             clearInterval(startInterval)
             selectRandom.classList.remove('selected')
         }, 2000)
     }
+
 }
 
-// function addSelectedClass() {
-//     this.classList.add('selected')
-// }
-// selectGreen.addEventListener('click', addSelectedClass)
-// selectYellow.addEventListener('click', addSelectedClass)
-// selectRed.addEventListener('click', addSelectedClass)
-// selectBlue.addEventListener('click', addSelectedClass)
+selectGreen.addEventListener('click', buttonIsClicked)
+selectYellow.addEventListener('click', buttonIsClicked)
+selectRed.addEventListener('click', buttonIsClicked)
+selectBlue.addEventListener('click', buttonIsClicked)
 
 
 /* To start the game user click the start game button 
