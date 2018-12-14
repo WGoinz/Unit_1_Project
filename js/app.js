22ebee496ff72f08055196fab2c7c4d7117558d3
Walter Goins <wgoinz@gmail.com>
2018-12-14 11:23:25 -0500

    added animation setup for green div. setup js on click to perform animation. Added start game button and centered its position with the game.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 console.log("Start of Simon Says")
+
+let selectGreen = document.getElementById('green')
+function addSelectedClass() {
+    this.classList.add('selected')
+}
+selectGreen.addEventListener('click', addSelectedClass)
