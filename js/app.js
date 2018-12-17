d34f2206b03eeb2a67a041c61e1aee9874a93b64
Walter Goins <wgoinz@gmail.com>
2018-12-17 17:19:55 -0500

    changed the order of the functions and cleaned up the code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // console.log("Start of Simon Says")
 
+/* Declare variables that will be used in the project */
+
 let selectGreen = document.getElementById('green')
 let selectYellow = document.getElementById('yellow')
 let selectRed = document.getElementById('red')
@@ -15,10 +17,14 @@ let playerScore = 0
 let highScore = 0
 let difficultyBackground
 
+/* Setup empty arrays and establish the array for Random Colors to be chosen*/
+
 let playerSequence = []
 let cpuSequence = []
-let cpuChoose = ['green', 'red', 'blue', 'yellow']
+const cpuChoose = ['green', 'red', 'blue', 'yellow']
 let randomColor
+
+/* Add all audio files to the project and declare them as constants */
 
 const audioStart = new Audio('../sounds/start.mp3')
 const audioGreen = new Audio('../sounds/green_sound.mp3')
@@ -30,25 +36,27 @@ const continueGame = new Audio('../sounds/ready_go.mp3')
 const bgm = new Audio('../sounds/actionBGM.mp3')
 const evilLaugh = new Audio('../sounds/evil_laugh.mp3')
 
+/* Setup logic for choosing which difficulty the player wants */
+
 document.getElementById('easy').onclick = function () {
     difficulty = easy
     difficultyBackground = this
     this.style.backgroundColor = 'red'
-
 }
-
 document.getElementById('hard').onclick = function () {
     difficulty = hard
     difficultyBackground = this
     this.style.backgroundColor = 'red'
 }
-
 document.getElementById('god').onclick = function () {
     difficulty = god
     difficultyBackground = this
     this.style.backgroundColor = 'red'
 }
 
+/* Start Game function. This is only called when the game is started
+The pickRandomColor function is the main function of this program
+ */
 
 function startGame() {
     evilLaugh.pause()
@@ -58,7 +66,7 @@ function startGame() {
     flicker.style.backgroundColor = 'red'
     document.getElementById('startGameButton').style.backgroundColor = "red"
     bgm.play()
-    console.log('Game has begun')
+    // console.log('Game has begun')
     playerSequence = []
     cpuSequence = []
     playerScore = 0
@@ -71,14 +79,21 @@ function startGame() {
     }, 6000)
 }
 
+/* To start the game user click the start game button 
+Once clicked, the game will tell the user the game has started */
+let startButton = document.getElementById('startGameButton')
+startButton.addEventListener('click', startGame)
+
+/* This function controls when each div has its class toggled
+to give it the effect of being selected. The function goes through
+a loop and there is a setTimeout interval modified by the difficulty */
+
 function toggleClass() {
     for (i = 0; i < cpuSequence.length; i++) {
         let selectRandom = document.getElementById(cpuSequence[i])
         // console.log(selectRandom)
-
         setTimeout(function () {
-            console.log(i)
-
+            // console.log(i)
             // console.log(selectRandom)
             selectRandom.classList.toggle('selected')
             setTimeout(function () {
@@ -97,19 +112,23 @@ function toggleClass() {
     }
 }
 
+/* Selects a random color from the original cpuChoose array */
+
 function pickRandomColor() {
     randomColor = cpuChoose[Math.floor(Math.random() * cpuChoose.length)]
     cpuSequence.push(randomColor)
-    console.log('Random color selected is ' + randomColor)
+    // console.log('Random color selected is ' + randomColor)
     // console.log(index)
     setTimeout(function () {
         toggleClass()
     }, 1000)
-
 }
 
-function buttonIsClicked() {
+/* Checks which button has been clicked and toggles the 'clicked' class
+Based off of this class, it plays a specific audio file related to that class
+Calls the compareSequences function at the end */
 
+function buttonIsClicked() {
     let selectedButton = this
     selectedButton.classList.toggle('clicked')
     setTimeout(function () {
@@ -130,20 +149,22 @@ function buttonIsClicked() {
     // console.log('The check is ' + check)
 }
 
+/* Looks at each index of the cpuSequence and playerSequence array
+If the values are equal it continues the loop until it reaches the
+current total length of the array. If all values are equal at each index, 
+a new round is started */
+
 function compareSequences() {
     let nextLevel = false
     for (i = 0; i < playerSequence.length; i++) {
         if (cpuSequence[i] !== playerSequence[i]) {
             nextLevel = false
-            console.log("You picked incorrectly at step " + i)
+            // console.log("You picked incorrectly at step " + i)
             setTimeout(function () {
                 checkHighScore()
                 flicker.classList.remove('text-flicker-in-glow')
                 flicker.style.backgroundColor = 'white'
                 document.getElementById('startGameButton').style.backgroundColor = "white"
-                setTimeout(function () {
-                    difficultyBackground.style.backgroundColor = "white"
-                }, 200)
                 gameEnd.play()
                 bgm.pause()
                 setTimeout(function () {
@@ -152,8 +173,9 @@ function compareSequences() {
                 selectScore.innerHTML = 'LOSER!! FINAL SCORE = ' + playerScore
                 // alert('You lose, Press Start Game to play again')
             }, 1000)
+            difficultyBackground.style.backgroundColor = "white"
         } else if (cpuSequence.length === playerSequence.length) {
-            console.log('Right answer')
+            // console.log('Right answer')
             nextLevel = true
         }
     }
@@ -171,19 +193,8 @@ function compareSequences() {
     }
 }
 
-
-function playSound() {
-    if (randomColor === 'green') {
-        audioGreen.play()
-    } else if (randomColor === 'red') {
-        audioRed.play()
-    } else if (randomColor === 'blue') {
-        audioBlue.play()
-    } else {
-        audioYellow.play()
-    }
-
-}
+/* Checks the playerScore and compares it to the highScore
+If the highScore is smaller then it is replaced with the playerScore's value */
 
 function checkHighScore() {
     if (playerScore >= highScore) {
@@ -198,9 +209,6 @@ selectRed.addEventListener('click', buttonIsClicked)
 selectBlue.addEventListener('click', buttonIsClicked)
 
 
-/* To start the game user click the start game button 
-Once clicked, the game will tell the user the game has started */
-let startButton = document.getElementById('startGameButton')
-startButton.addEventListener('click', startGame)
+
 
 
