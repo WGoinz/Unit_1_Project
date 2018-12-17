272a203519b22c2c09a94fe6d86f126ca3bf1d5a
Walter Goins <wgoinz@gmail.com>
2018-12-17 16:23:50 -0500

    added high score functionality and tested to make sure it works.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log("Start of Simon Says")
+// console.log("Start of Simon Says")
 
 let selectGreen = document.getElementById('green')
 let selectYellow = document.getElementById('yellow')
@@ -12,6 +12,7 @@ let hard = 800
 let god = 150
 let difficulty = 1500
 let playerScore = 0
+let highScore = 0
 let difficultyBackground
 
 let playerSequence = []
@@ -19,6 +20,15 @@ let cpuSequence = []
 let cpuChoose = ['green', 'red', 'blue', 'yellow']
 let randomColor
 
+const audioStart = new Audio('../sounds/start.mp3')
+const audioGreen = new Audio('../sounds/green_sound.mp3')
+const audioYellow = new Audio('../sounds/yellow_sound.mp3')
+const audioBlue = new Audio('../sounds/punch2.mp3')
+const audioRed = new Audio('../sounds/punch1.mp3')
+const gameEnd = new Audio('../sounds/you_lose.mp3')
+const continueGame = new Audio('../sounds/ready_go.mp3')
+const bgm = new Audio('../sounds/actionBGM.mp3')
+const evilLaugh = new Audio('../sounds/evil_laugh.mp3')
 
 document.getElementById('easy').onclick = function () {
     difficulty = easy
@@ -39,16 +49,6 @@ document.getElementById('god').onclick = function () {
     this.style.backgroundColor = 'red'
 }
 
-
-const audioStart = new Audio('../sounds/start.mp3')
-const audioGreen = new Audio('../sounds/green_sound.mp3')
-const audioYellow = new Audio('../sounds/yellow_sound.mp3')
-const audioBlue = new Audio('../sounds/punch2.mp3')
-const audioRed = new Audio('../sounds/punch1.mp3')
-const gameEnd = new Audio('../sounds/you_lose.mp3')
-const continueGame = new Audio('../sounds/ready_go.mp3')
-const bgm = new Audio('../sounds/actionBGM.mp3')
-const evilLaugh = new Audio('../sounds/evil_laugh.mp3')
 
 function startGame() {
     evilLaugh.pause()
@@ -137,6 +137,7 @@ function compareSequences() {
             nextLevel = false
             console.log("You picked incorrectly at step " + i)
             setTimeout(function () {
+                checkHighScore()
                 flicker.classList.remove('text-flicker-in-glow')
                 flicker.style.backgroundColor = 'white'
                 document.getElementById('startGameButton').style.backgroundColor = "white"
@@ -148,13 +149,9 @@ function compareSequences() {
                 setTimeout(function () {
                     evilLaugh.play()
                 }, 1500)
-                selectScore.innerHTML = 'LOSER: FINAL SCORE = ' + playerScore
+                selectScore.innerHTML = 'LOSER!! FINAL SCORE = ' + playerScore
                 // alert('You lose, Press Start Game to play again')
             }, 1000)
-            // setTimeout(function () {
-            //     continueGame.play()
-            //     let playAgain = prompt('Would you like to play again?')
-            // }, 2000)
         } else if (cpuSequence.length === playerSequence.length) {
             console.log('Right answer')
             nextLevel = true
@@ -162,6 +159,7 @@ function compareSequences() {
     }
     if (nextLevel === true) {
         playerScore++
+        checkHighScore()
         selectScore.innerHTML = 'CURRENT SCORE = ' + playerScore
         playerSequence = []
         setTimeout(function () {
@@ -187,25 +185,12 @@ function playSound() {
 
 }
 
-// function showSequence(index) {
-//     let selectRandom = document.getElementById(cpuSequence[index])
-//     selectRandom.classList.toggle('selected')
-//     setTimeout(function () {
-//         selectRandom.classList.toggle('selected')
-//     }, 500)
-//     playSound()
-// console.log(selectRandom)
-// function addSelectedClass() {
-//     selectRandom.classList.add('selected')
-// }
-// let startInterval = setInterval(addSelectedClass, 500)
-// setTimeout(function () {
-//     clearInterval(startInterval)
-//     selectRandom.classList.remove('selected')
-// }, (i + 1) * 1000)
-
-// }
-
+function checkHighScore() {
+    if (playerScore >= highScore) {
+        highScore = playerScore
+        document.getElementById('highScore').innerHTML = 'HIGH SCORE = ' + highScore
+    }
+}
 
 selectGreen.addEventListener('click', buttonIsClicked)
 selectYellow.addEventListener('click', buttonIsClicked)
